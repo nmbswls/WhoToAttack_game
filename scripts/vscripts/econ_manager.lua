760bc82aaf0d22f452ab9a032d6f65a92279b52c
unknown <nmbswls@163.com>
2021-01-20 14:02:54 +0800

    delete

--- scripts/vscripts/econ_manager.lua
+++ scripts/vscripts/econ_manager.lua
@@ -2,13 +2,13 @@
 if EconManager == nil then EconManager = class({}) end
 
 function EconManager:OnPlayerQueryShopItemsReq(keys)
-	if self.vEconItems == nil then 
-		HttpUtils:SendHTTPGet('http://yueyutech.com:10010/GetShopItems',function(result)
-			if result.StatusCode == 200 then
-				local items = JSON:decode(result.Body)
-				self.vEconItems = items
-				CustomNetTables:SetTableValue('econ_data', 'shop_items', items)
-			end
-		end);
-	end
+	--if self.vEconItems == nil then 
+	--	HttpUtils:SendHTTPGet('http://yueyutech.com:10010/GetShopItems',function(result)
+	--		if result.StatusCode == 200 then
+	--			local items = JSON:decode(result.Body)
+	--			self.vEconItems = items
+	--			CustomNetTables:SetTableValue('econ_data', 'shop_items', items)
+	--		end
+	--	end);
+	--end
 end
