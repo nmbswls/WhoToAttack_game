4b232745dca11c59b6a740496bb718106acd17ab
unknown <nmbswls@163.com>
2021-01-25 19:46:54 +0800

    econ add something

--- scripts/vscripts/econ_manager.lua
+++ scripts/vscripts/econ_manager.lua
@@ -11,6 +11,10 @@ function EconManager:constructor()
 		self:OnPlayerQueryShopItemsReq(keys)
 	end)
 	
+	CustomGameEventManager:RegisterListener("player_query_econ_data_req",function(_, keys)
+		self:OnPlayerQueryEconData(keys)
+	end)
+	
 end
 function EconManager:OnPlayerQueryShopItemsReq(keys)
 	print('OnPlayerQueryShopItemsReq')
@@ -31,6 +35,47 @@ function EconManager:OnPlayerQueryShopItemsReq(keys)
 	--		end
 	--	end);
 	--end
+end
+
+function EconManager:OnPlayerQueryEconData(keys)
+	print('OnPlayerQueryEconInfo')
+	
+	local playerid = keys.PlayerID
+	local player = PlayerResource:GetPlayer(playerid)
+	if not player then return end
+	local hero = player:GetAssignedHero()
+	if not hero then return end
+	local data = {
+		[1] = "t10",
+		[2] = "t11",
+	}
+	
+	CustomNetTables:SetTableValue('econ_data', 'collection_data_' .. playerid, data)
+	CustomNetTables:SetTableValue('econ_data', 'coin_data_' .. playerid, {amount = 10})
+	
+	return;
+	local steamid = PlayerResource:GetSteamAccountID(playerid)
+	local req = CreateHTTPRequestScriptVM("POST","http://yueyutech.com:10010/GetCollection")
+	req:SetHTTPRequestGetOrPostParameter("steamid",tostring(steamid))
+	req:Send(function(result)
+		if result.StatusCode == 200 then
+			local data = JSON:decode(result.Body)
+			for name, equip in pairs(data) do
+				if name ~= 'steamid' then
+					if equip == true then
+						if Econ["OnEquip_" .. name .. "_server"] then
+							Econ["OnEquip_" .. name .. "_server"](hero)
+						end
+					else
+						if Econ["OnRemove_" .. name .. "_server"] then
+							Econ["OnRemove_" .. name .. "_server"](hero)
+						end
+					end
+				end
+			end
+			CustomNetTables:SetTableValue('econ_data', 'collection_data_' .. playerid, data)
+		end
+	end)
 end
 
 if GameRules.EconManager == nil then GameRules.EconManager = EconManager() end
