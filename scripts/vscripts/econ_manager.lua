228be4479095dbd11cdc668ad238311aafaf1294
unknown <nmbswls@163.com>
2021-01-25 21:43:53 +0800

    add econ more

--- scripts/vscripts/econ_manager.lua
+++ scripts/vscripts/econ_manager.lua
@@ -1,6 +1,44 @@
 
 if not IsServer() then return end
 
+
+function EconRemoveParticle(pid)
+	ParticleManager:DestroyParticle(pid, true)
+	ParticleManager:ReleaseParticleIndex(pid)
+end
+
+function EconCreateParticleOverhead(hero, particleName)
+	local pid = ParticleManager:CreateParticle(particleName,PATTACH_OVERHEAD_FOLLOW,hero)
+	ParticleManager:SetParticleControlEnt(pid,0,hero,PATTACH_OVERHEAD_FOLLOW,"follow_overhead",hero:GetAbsOrigin(),true)
+	return pid
+end
+
+EconFuncs = {}
+
+EconFuncs.OnEquip_e101_server = function(hero)
+	WhoToAttack:ChangeBaseModel(hero, "")
+end
+
+EconFuncs.OnRemove_e101_server = function(hero)
+	WhoToAttack:ChangeBaseModel(hero, "")
+end
+
+EconFuncs.OnEquip_e102_server = function(hero)
+	WhoToAttack:ChangeBaseModel(hero, "")
+end
+
+EconFuncs.OnRemove_e102_server = function(hero)
+	WhoToAttack:ChangeBaseModel(hero, "")
+end
+
+EconFuncs.OnEquip_e103_server = function(hero)
+	WhoToAttack:ChangeBaseModel(hero, "")
+end
+
+EconFuncs.OnRemove_e103_server = function(hero)
+	WhoToAttack:ChangeBaseModel(hero, "")
+end
+
 if EconManager == nil then EconManager = class({}) end
 
 print('require econ manager')
@@ -14,6 +52,11 @@ function EconManager:constructor()
 	CustomGameEventManager:RegisterListener("player_query_econ_data_req",function(_, keys)
 		self:OnPlayerQueryEconData(keys)
 	end)
+	
+	CustomGameEventManager:RegisterListener("player_equip_req",function(_, keys)
+		self:OnPlayerEquip(keys)
+	end)
+	
 	
 end
 function EconManager:OnPlayerQueryShopItemsReq(keys)
@@ -77,5 +120,75 @@ function EconManager:OnPlayerQueryEconData(keys)
 		end
 	end)
 end
+
+
+function EconManager:OnPlayerEquip(keys)
+	local playerid = keys.PlayerID
+	local steamid = PlayerResource:GetSteamAccountID(playerid)
+	local to_equip = keys.to_equip
+	local slot = 1  -- read config
+	local equips = GetTableValue('econ_data', 'equip_info_' .. playerid);
+	
+	local hero = player:GetAssignedHero()
+	if not hero then return end
+	
+	
+	local oldEquip = equips[slot];
+	equips[slot] = to_equip;
+	
+	CustomNetTables:SetTableValue('econ_data', 'equip_info_' .. playerid, equips)
+	
+	--jiaoyan
+	if to_equip ~= nil then
+		if EconFuncs["OnEquip_" .. to_equip .. "_server"] then
+			EconFuncs["OnEquip_" .. to_equip .. "_server"](hero)
+		end
+	end
+	
+	if oldEquip ~= nil and EconFuncs["OnEquip_" .. oldEquip .. "_server"]  then
+		EconFuncs["OnEquip_" .. oldEquip .. "_server"](hero)
+	end
+	
+end
+
+
+
+function EconManager:OnPlayerPreview(keys)
+	local playerid = keys.PlayerID
+	local player = PlayerResource:GetPlayer(playerid)
+	if not player then return end
+	local hero = player:GetAssignedHero()
+	if not hero then return end
+
+	local name = keys.item
+
+	if Econ["OnEquip_" .. name .. "_server"] then
+		Econ["OnEquip_" .. name .. "_server"](hero)
+	end
+
+	Timer(10, function()
+		Econ["OnRemove_" .. name .. "_server"](hero)
+	end)
+end
+
+
+function EconManager:OnPlayerPurchase(keys)
+	local id = keys.PlayerID
+	local item = keys.ItemName
+	local steamid = PlayerResource:GetSteamAccountID(id)
+	
+	return;
+	
+	local req = CreateHTTPRequestScriptVM('POST', 'http://yueyutech.com:10010/Purchase')
+	req:SetHTTPRequestGetOrPostParameter('steamid', tostring(steamid))
+	req:SetHTTPRequestGetOrPostParameter('item', tostring(item))
+	req:Send(function(result)
+		if result.StatusCode == 200 then
+			CustomGameEventManager:Send_ServerToPlayer(PlayerResource:GetPlayer(id), 'bom_player_purchase_message', {})
+		end
+		self:OnPlayerAskCollection({PlayerID=id})
+	end)
+end
+
 
 if GameRules.EconManager == nil then GameRules.EconManager = EconManager() end
